509c48a19acab6aa4483f69284ee50aabd807d39
tahasidd09 <161510569+tahasidd09@users.noreply.github.com>
2024-09-15 15:25:23 +0500

    Delete script.ts

--- script.ts
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toggleSkillsButton = document.getElementById("toggle-skills") as HTMLButtonElement;
-    const skillsList = document.getElementById("skills-list") as HTMLUListElement;
-
-    toggleSkillsButton.addEventListener("click", () => {
-        skillsList.style.display = skillsList.style.display === "block" ? "none" : "block";
-    });
-});
